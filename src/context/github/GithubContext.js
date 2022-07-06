01361d60867501c42627fe25fd98526215170364
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-07-06 12:41:38 +0100

    Adding clear users method

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -49,6 +49,8 @@ export const GithubProvider = ({ children }) => {
 
     }
 
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
     // Set loading
     const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
@@ -56,6 +58,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
