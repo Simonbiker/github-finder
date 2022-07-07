772b0ea2e6c46e27e86a6aaed9145620a9731fb2
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-07-07 09:37:19 +0100

    Adding user component. Set up for getting one user from github

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false,
     }
 
@@ -49,6 +50,28 @@ export const GithubProvider = ({ children }) => {
 
     }
 
+    // Getting single user
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if (response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+
+    }
+
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
     // Set loading
@@ -57,7 +80,9 @@ export const GithubProvider = ({ children }) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
+        getUser,
         clearUsers
     }}>
         {children}
